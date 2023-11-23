b539675bc3fa8ae104969bbec02e27e5389ca416
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-11-23 00:47:27 -0800

    Created sample arduino program to read UART data and parse it on the MCU

---
+++ microcontroller_code/LED_blink_test/LED_blink_test.ino
@@ -0,0 +1,37 @@
+char receivedChar;
+bool newData;
+int ledState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.print("Arduino Ready!");
+
+  // interrupts();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(1000);
+  // digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, ledState);
+  // delay(1000);
+
+  if(Serial.available() > 0){
+    receivedChar = Serial.read();
+    newData = true;
+  }  
+
+  if(newData){
+    Serial.print("Received byte: ");
+    Serial.println(receivedChar);
+    if(receivedChar == '0')
+      ledState = LOW;
+    else if(receivedChar == '1')
+      ledState = HIGH;
+    newData = false;
+  }
+}
